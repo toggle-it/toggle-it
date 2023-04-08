ad9fdd6773ea1cfff25ca41e45f34bf7162c7064
Pankaj Chauhan <Pankaj3960x@gmail.com>
2023-04-07 19:08:54 -0700

    feat: configure fastify, jwt, swagger

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   root: true,
   extends: ["@ti/eslint-config"],
+  rules: {
+    "@next/next/no-assign-module-variable": "off",
+  },
 };
--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { configuration } from "config/configuration";
+
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ load: [configuration] }), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
