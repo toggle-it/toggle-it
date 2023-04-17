700261bee229374984be89547ed58b292e539acc
Pankaj Chauhan <Pankaj3960x@gmail.com>
2023-04-17 08:23:56 -0700

    feat(storybook): add app `extras-design-system`

---
+++ apps/x-design-system/stories/Button.stories.js
@@ -0,0 +1,21 @@
+import { Button } from "@ti/ui";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+export default {
+  title: "Example/Button",
+  component: Button,
+  tags: ["autodocs"],
+  argTypes: {
+    backgroundColor: {
+      control: "color",
+    },
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary = {
+  args: {
+    primary: true,
+    label: "Button",
+  },
+};
